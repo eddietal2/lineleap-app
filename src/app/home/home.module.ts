3c3cc2eeb93c94bfc706e69ad50d04c0f46c94fb
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2023-11-17 17:05:24 -0500

    Added correct SVG icons to Tabbar)

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
-import { HomePageRoutingModule } from './home-page-routing.module';
+import { HomePageRoutingModule } from './home-routing.module';
 
 @NgModule({
   imports: [
